6424fcbdce316f9c9e97a05712e8ed2ec1f13d24
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-20 13:52:53 -0500

    Updated home controller and index to correctly organize the top 10 grossing books

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @books = Book.includes(:publisher).order("sale_rank").limit(10)
+    @books = Book.includes(:publisher).order("sale_rank")
 
     @publishers = Publisher.order_by_book_count.includes(:books).all
   end
