9ac7d8f74488308486ef8e9e611ed76c74b98f44
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-19 23:58:49 -0500

    Updated the root route to point to the home controller's index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "home/index"
+  root to: "home#index"
   resources :publishers, only: [ :index, :show ]
   resources :books, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
