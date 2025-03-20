c4827414a078d5522fee5408c9df3de8677ad97b
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-19 20:34:32 -0500

    Updated seed script and created Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,23 @@ books.each do |book|
   publisher = Publisher.find_or_create_by(publisher_name: book["Publisher"])
 
   if publisher.valid?
-    # create the book
+    new_book = publisher.books.create(
+      book_name: book["Book_Name"],
+      publishing_year: book["Publishing_Year"],
+      language_code: book["Language_Code"],
+      book_average_rating: book["Book_Average_Rating"],
+      book_ratings_count: book["Book_Ratings_Count"],
+      genre: book["Genre"],
+      gross_sales: book["Gross_Sales"],
+      sale_price: book["Sale_Price"],
+      sale_rank: book["Sale_Rank"],
+      units_sold: book["Units_Sold"]
+    )
+    puts "Unable to create the book #{book["Book_Name"]}" unless new_book.valid?
   else
     puts "Unable to create the publisher name #{book["Publisher"]}"
   end
 end
 
 puts "Created #{Publisher.count} Publishers"
+puts "Created #{Book.count} Books"
