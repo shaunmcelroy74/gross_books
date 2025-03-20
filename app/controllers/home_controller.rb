efaf0527c46a936fe88f3442a13ab466a7b46488
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-20 00:19:08 -0500

    Fixed n+1 problem on the home page for book's publisher

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @books = Book.order("sale_rank").limit(10)
+    @books = Book.includes(:publisher).order("sale_rank").limit(10)
 
     @publishers = Publisher.order("publisher_name ASC").all
   end
