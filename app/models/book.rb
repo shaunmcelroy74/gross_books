4f9609d45ba7d92a854953a94f44711c33d9544e
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-19 19:49:02 -0500

    Added validations for Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :publisher
+
+  validates :book_name, :publishing_year, :language_code, :book_average_rating, :book_ratings_count,
+  :genre, :gross_sales, :sale_price, :sale_rank, :units_sold, presence: true
+  validates :book_name, uniqueness: true
+  validates :publishing_year, :book_ratings_count, :sale_rank, :units_sold, numericality: { only_integer: true }
+  validates :book_average_rating, numericality: true
 end
