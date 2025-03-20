666d949c8fc9baa28dbe741350fcb0045bf8666c
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-19 23:53:47 -0500

    Added a home controller with index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :publishers, only: [ :index, :show ]
   resources :books, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
