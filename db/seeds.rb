ce0ab6874198262f23f2ef1ebd0f7929611843c1
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-19 20:20:09 -0500

    Parsing the bookPublishingData.csv file and creating Publishers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+Book.destroy_all
+Publisher.destroy_all
+
+filename = Rails.root.join("db/bookPublishingData.csv")
+
+puts "Reading in the file from here #{filename}"
+
+csv_data = File.read(filename)
+books = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+books.each do |book|
+  publisher = Publisher.find_or_create_by(publisher_name: book["Publisher"])
+
+  if publisher.valid?
+    # create the book
+  else
+    puts "Unable to create the publisher name #{book["Publisher"]}"
+  end
+end
+
+puts "Created #{Publisher.count} Publishers"
