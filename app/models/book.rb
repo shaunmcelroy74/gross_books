9cd3277671b30a68bc8edb93cff941131c78a762
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-20 17:05:36 -0500

    Added the relationships to the Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 
   validates :book_name, :publishing_year, :language_code, :book_average_rating, :book_ratings_count,
   :genre, :gross_sales, :sale_price, :sale_rank, :units_sold, presence: true
