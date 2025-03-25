9d3d41dacc12867c31733a26faa799a767de7bb6
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-25 16:54:54 -0500

    Trying to fix CI

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,15 +3,18 @@ require "test_helper"
 class PagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @page = pages(:one)
+    @auth_headers = {
+      "HTTP_AUTHORIZATION" => ActionController::HttpAuthentication::Basic.encode_credentials("admin", "secret")
+    }
   end
 
   test "should get index" do
-    get pages_url
+    get pages_url, headers: @auth_headers
     assert_response :success
   end
 
   test "should get new" do
-    get new_page_url
+    get new_page_url, headers: @auth_headers
     assert_response :success
   end
 
@@ -21,19 +24,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
         title: "New Unique Title",
         content: "New unique content",
         permalink: "unique-permalink-123"
-      } }
+      } }, headers: @auth_headers
     end
 
     assert_redirected_to page_url(Page.last)
   end
 
   test "should show page" do
-    get page_url(@page)
+    get page_url(@page), headers: @auth_headers
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_page_url(@page)
+    get edit_page_url(@page), headers: @auth_headers
     assert_response :success
   end
 
@@ -42,13 +45,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
       title: "Updated Unique Title",
       content: "Updated unique content",
       permalink: "updated-unique-permalink-456"
-    } }
+    } }, headers: @auth_headers
     assert_redirected_to page_url(@page)
   end
 
   test "should destroy page" do
     assert_difference("Page.count", -1) do
-      delete page_url(@page)
+      delete page_url(@page), headers: @auth_headers
     end
 
     assert_redirected_to pages_url
